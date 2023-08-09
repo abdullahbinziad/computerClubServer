dd74ca1a17bb7a09661b7d01664aca7d38e4d5d8
abdullahbinziad <abdullahbinziad@gmail.com>
2023-08-09 10:06:00 +0600

    add soial link done

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+
+
+const corsConfig = {
+
+  origin:"*",
+  Credential: true ,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+}
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
+
+// verify Jwt
+// verify the JWT
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unAuthorized Access" });
+  }
+  //bearre token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECCRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "Unauthorized Access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 
 const uri = `mongodb://127.0.0.1:27017/clubDB`;
 
@@ -26,8 +64,57 @@ async function run() {
   try {
     const database = client.db("clubDB");
     const executivesCollection = database.collection("executives");
+    const memberCollection = database.collection("members");
 
     // making api
+
+
+ //post jwt request from headers
+
+ app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECCRET, {
+    expiresIn: "1h",
+  });
+  console.log("the generated token", token);
+  res.send({ token });
+});
+
+
+//warning: use verify jwt before using verify member
+    const verifyMember = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await memberCollection.findOne(query);
+
+      if (user?.role !== "member") {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden message" });
+      }
+      next();
+    };
+
+
+
+//for hook verify 
+
+//check is it member --true/false 
+app.get('/users/member/:email',verifyJWT, async (req, res) => {
+  const email = req.params.email;
+
+  if (req.decoded.email !== email) {
+    res.send({ member: false })
+  }
+  const query = { email: email }
+  const user = await memberCollection.findOne(query);
+  const result = { member: user?.role === 'member' }
+  res.send(result);
+})
+
+
+
+
 
     //get api of executive
     app.get("/executives", async (req, res) => {
@@ -75,6 +162,10 @@ async function run() {
           email: data.email,
           mobile: data.mobile,
           image: data.image,
+          aboutDesc: data.aboutDesc,
+          fb: data.fb,
+          whatsapp: data.whatsapp,
+          linkdin: data.linkdin
         },
       };
       const result = await executivesCollection.updateOne(
@@ -84,6 +175,117 @@ async function run() {
       );
       res.send(result);
     });
+
+
+//member appi
+
+//create a member 
+
+app.post('/createmember', async (req, res) => {
+  const user = req.body;
+const query = { email: user.email }
+const existingUser = await memberCollection.findOne(query);
+
+if (existingUser) {
+  return res.send({ message: 'user already exists' })
+}
+  const result = await memberCollection.insertOne(user);
+  res.send(result);
+})
+
+//get the member data 
+
+app.get('/member', async (req, res) => {
+ const filter = req.query ;
+ if (filter && filter.email) {
+  query = { email: filter.email };
+}
+console.log(query);
+const result = await memberCollection.find(query).toArray() ;
+  res.send(result);
+
+})
+
+
+//update Member Info 
+app.put("/userInfo/:email", async (req, res) => {
+  const email = req.params.email;
+ 
+const query = { email: email };
+
+ const user = await memberCollection.findOne(query);
+ const filter=  user
+console.log(filter);
+  const data = req.body;
+  const option = { upsert: true };
+  const updateDoc = {
+    $set: {
+    fullname: data.fullname,
+    department: data.department,
+    batch: data.batch,
+    session: data.session,
+    studentID: data.studentID,
+    },
+  };
+  const result = await memberCollection.updateOne(
+    filter,
+    updateDoc,
+    option
+  );
+  res.send(result);
+});
+
+//update contact 
+app.put("/userContact/:email", async (req, res) => {
+  const email = req.params.email;
+ 
+const query = { email: email };
+
+ const user = await memberCollection.findOne(query);
+ const filter=  user
+console.log(filter);
+  const data = req.body;
+  const option = { upsert: true };
+  const updateDoc = {
+    $set: {
+    mobile: data.mobile,
+    fulladdress: data.fulladdress,
+    },
+  };
+  const result = await memberCollection.updateOne(
+    filter,
+    updateDoc,
+    option
+  );
+  res.send(result);
+});
+//update soocial 
+
+
+app.put("/userSocial/:email", async (req, res) => {
+  const email = req.params.email;
+ 
+const query = { email: email };
+
+
+  const newSocialLinks = req.body;
+  const result = await memberCollection.findOneAndUpdate(
+    { email: email },
+    { $set: { socialLink: newSocialLinks } },
+    { returnOriginal: false }
+  );
+
+  if (!result.value) {
+    return res.status(404).json({ message: 'Document not found' });
+  }
+  res.json({ message: 'Social links updated successfully', document: result.value });
+
+});
+
+
+
+
+
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
